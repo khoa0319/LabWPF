5e667aa620f61dae971f3a90b705d6afd29de505
khoa0319 <khoa0319@gmail.com>
2018-03-27 12:05:50 +0700

    exercise 4 added, exercise 3 modified

---
+++ ThucHanh4/ThucHanh4/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThucHanh4
+{
+    public class Employee
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Address { get; set; }
+
+        public Employee(string id, string name, DateTime date, string address)
+        {
+            ID = id;
+            Name = name;
+            BirthDate = date;
+            Address = address;
+        }
+        public Employee()
+        {
+
+        }
+
+        public static List<Employee> GetSampleData() => new List<Employee>
+        {
+            new Employee("16DH110001", "Nguyễn Văn A", new DateTime(1998, 11, 18), "Gò Công"),
+            new Employee("16DH110002", "Nguyễn Văn B", new DateTime(1999, 2, 18), "Gò Công"),
+            new Employee("16DH110003", "Nguyễn Văn C", new DateTime(1998, 3, 18), "Gò Công"),
+            new Employee("16DH110004", "Nguyễn Văn D", new DateTime(1998, 2, 12), "Gò Công"),
+            new Employee("16DH110005", "Nguyễn Văn E", new DateTime(1998, 2, 14), "Gò Công"),
+            new Employee("16DH110006", "Nguyễn Văn F", new DateTime(1998, 2, 15), "Gò Công"),
+            new Employee("16DH110010", "Nguyễn Văn J", new DateTime(1998, 9, 28), "Gò Công"),
+            new Employee("16DH110007", "Nguyễn Văn G", new DateTime(1998, 5, 18), "Gò Công"),
+            new Employee("16DH110008", "Nguyễn Văn H", new DateTime(1998, 2, 08), "Gò Công"),
+            new Employee("16DH110009", "Nguyễn Văn I", new DateTime(1998, 8, 18), "Gò Công"),
+            new Employee("16DH110001", "Nguyễn Văn A", new DateTime(1998, 11, 18), "Gò Công"),
+            new Employee("16DH110002", "Nguyễn Văn B", new DateTime(1999, 2, 18), "Gò Công"),
+            new Employee("16DH110003", "Nguyễn Văn C", new DateTime(1998, 3, 18), "Gò Công"),
+            new Employee("16DH110004", "Nguyễn Văn D", new DateTime(1998, 2, 12), "Gò Công"),
+            new Employee("16DH110005", "Nguyễn Văn E", new DateTime(1998, 2, 14), "Gò Công"),
+            new Employee("16DH110006", "Nguyễn Văn F", new DateTime(1998, 2, 15), "Gò Công"),
+            new Employee("16DH110010", "Nguyễn Văn J", new DateTime(1998, 9, 28), "Gò Công"),
+            new Employee("16DH110007", "Nguyễn Văn G", new DateTime(1998, 5, 18), "Gò Công"),
+            new Employee("16DH110008", "Nguyễn Văn H", new DateTime(1998, 2, 08), "Gò Công"),
+            new Employee("16DH110009", "Nguyễn Văn I", new DateTime(1998, 8, 18), "Gò Công")
+        };
+    }
+}
---
+++ ThucHanh4/ThucHanh4/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ThucHanh4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Employee> lst;
+        private Employee stored;
+        public MainWindow()
+        {
+            InitializeComponent();
+            //lst = new List<Employee>();
+            lst = Employee.GetSampleData();
+            dtEmployee.ItemsSource = lst;
+
+            //create a employee
+            btnCreate.Click += (sender, e) =>
+            {
+                Employee em = new Employee
+                {
+                    ID = txtID.Text,
+                    Name = txtName.Text,
+                    BirthDate = (DateTime)datePicker.SelectedDate,
+                    Address = txtAddress.Text
+                };
+                lst.Add(em);
+                ResetInputField();
+                dtEmployee.Items.Refresh();
+            };
+
+            //remove an employee
+            btnDelete.Click += (sender, e) =>
+            {
+                if (dtEmployee.SelectedItem == null)
+                {
+                    MessageBox.Show("bạn hãy chọn ô cần xóa");
+                    return;
+                }
+                lst.Remove(lst.Find(em => em.ID == ((Employee)dtEmployee.SelectedItem).ID));
+                dtEmployee.Items.Refresh();
+            };
+
+            dtEmployee.SelectionChanged += (sender, e) =>
+            {
+                if (dtEmployee.SelectedItem != null)
+                {
+                    stored = dtEmployee.SelectedItem as Employee;
+                    Employee emp = dtEmployee.SelectedItem as Employee;
+                    txtID.Text = emp.ID;
+                    txtName.Text = emp.Name;
+                    txtAddress.Text = emp.Address;
+                    datePicker.SelectedDate = emp.BirthDate;
+                }
+            };
+
+            //update an employee
+            btnUpdate.Click += (sender, e) =>
+            {
+                if (dtEmployee.SelectedItem == null)
+                {
+                    MessageBox.Show("Hãy chọn nhân viên để cập nhật");
+                    return;
+                }
+                stored.ID = txtID.Text;
+                stored.Name = txtName.Text;
+                stored.Address = txtAddress.Text;
+                stored.BirthDate = (DateTime)datePicker.SelectedDate;
+                dtEmployee.Items.Refresh();
+            };
+           
+            //escape the program
+            btnExit.Click += (sender, e) =>
+            {
+                MessageBoxResult result = MessageBox.Show("Chấp nhận thoát", "Exit program", MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.No);
+                if (result == MessageBoxResult.Yes)
+                    this.Close();
+            };
+        }
+
+        private void ResetInputField()
+        {
+            txtAddress.Text = "";
+            txtID.Text = "";
+            txtName.Text = "";
+            datePicker.DisplayDate = DateTime.Today;
+        }
+    }
+}
